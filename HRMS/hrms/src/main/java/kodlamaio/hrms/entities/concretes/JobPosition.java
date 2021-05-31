be6cf3657fef9563a20c9e9d1445d1121649c972
SevvalAcet <83153151+SevvalAcet@users.noreply.github.com>
2021-05-31 10:08:20 +0300

    Job Positions

---
+++ HRMS/hrms/src/main/java/kodlamaio/hrms/entities/concretes/JobPosition.java
@@ -0,0 +1,37 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="job_positions")
+public class JobPosition {
+
+@Id
+@GeneratedValue
+@Column(name="id")
+private int id;
+
+@Column(name="userId")
+private int userId;
+
+@Column(name="name")
+private String name;
+
+
+public JobPosition(int id, int userId, String name) {
+	super();
+	this.id = id;
+	this.userId = userId;
+	this.name = name;
+}
+
+
+
+}
